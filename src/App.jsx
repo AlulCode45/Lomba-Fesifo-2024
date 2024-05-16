7390458eb34abb52dd7f837c3e661dc6d27295d0
AlulCode45 <alulgans70@gmail.com>
2024-05-16 22:32:06 +0700

    "Refactor Accordion component styles and layout"

--- src/App.jsx
+++ src/App.jsx
@@ -534,47 +534,25 @@ export default function App() {
           </div>
         </section>
 
-        <section className='mt-24'>
+        <section className='mt-24 px-24'>
           <div id="accordionExample">
-            <div className="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+            <div className="my-4">
               <h2 className="mb-0" id="headingOne">
                 <button
                   className={`${activeElement === "element1" &&
-                    `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 dark:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]`
-                    } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white`}
-                  type="button"
-                  onClick={() => handleClick("element1")}
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
+                    `[box-shadow:inset_0_-1px_0_rgba(75,85,99)]`
+                    } group relative flex w-full items-center rounded-t-[15px] px-5 py-4 text-left text-base transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none text-white border-b font-bold`} type="button" onClick={() => handleClick("element1")} aria-expanded="true" aria-controls="collapseOne">
                   Accordion Item #1
                   <span
                     className={`${activeElement === "element1"
                       ? `rotate-[-180deg] -mr-1`
                       : `rotate-0 fill-[#212529]  dark:fill-white`
-                      } ml-auto h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="h-6 w-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                      />
-                    </svg>
+                      } ml-auto h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg>
                   </span>
                 </button>
               </h2>
-              <TECollapse
-                show={activeElement === "element1"}
-                className="!mt-0 !rounded-b-none !shadow-none"
-              >
+              <TECollapse show={activeElement === "element1"} className="!mt-0 !rounded-b-none !shadow-none">
                 <div className="px-5 py-4">
                   <strong>This is the first item's accordion body.</strong> Lorem
                   ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu
@@ -588,7 +566,7 @@ export default function App() {
             </div>
           </div>
         </section>
-      </main>
+      </main >
 
       <footer>
         <div class="text-white py-10">
